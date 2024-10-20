ea36ae2617d56fcf7fb2cc1783859b014a8fb373
Harsh <hmaghnani6@gmail.com>
2024-10-20 14:04:31 +0500

    Update Question08.c

--- Assignments/Assignment2/Question08.c
+++ Assignments/Assignment2/Question08.c
@@ -2,10 +2,10 @@
 #include <math.h>
 #include <string.h>
 
-int BinaryToDecimal(int num) {
-    int decimal = 0;  
-    int i = 0; 
-    int data;
+long BinaryToDecimal(long num) {
+    long decimal = 0;  
+    long i = 0; 
+    long data;
 
     while (num > 0) {
         data = num % 10;
@@ -19,11 +19,11 @@ int BinaryToDecimal(int num) {
     return decimal;
 }
 
-int DecimalToBinary(int num) {
+long DecimalToBinary(long num) {
     char binary[1000000] = {'\0'}; 
-    int power = num;  
-    int index = 0; 
-    int count = 0;
+    long power = num;  
+    long index = 0; 
+    long count = 0;
     while (power >= 0) {
         if (pow(2, power) <= num) {
             num = num - pow(2, power);  
@@ -40,9 +40,9 @@ int DecimalToBinary(int num) {
     
     long number=0;
     long multiplier=1;
-    int i;
-    int count1 = count;
-    int counter=1;
+    long i;
+    long count1 = count;
+    long counter=1;
     for(i=index-1;i>=index-count1;i--){
         if(binary[i] == '1'){
             number = number + 1*multiplier;
@@ -54,47 +54,85 @@ int DecimalToBinary(int num) {
 
 }
 
-void HexadecimalToBinary(char arr[]){
-    int size = strlen(arr);
-    char decimal[1000000]={'\0'};
-    int num,i;
+long HexadecimalToBinary(char arr[]){
+    long size = strlen(arr);
+    char binary[1000000]={'\0'};
+    long num,i;
     char str[5];
     for(i=0;i<size;i++){
         if(arr[i]=='A' || arr[i]=='a'){
             num = 10;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else if(arr[i]=='B' || arr[i]=='b'){
             num = 11;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else if(arr[i]=='C' || arr[i]=='c'){
             num = 12;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else if(arr[i]=='D' || arr[i]=='d'){
             num = 13;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else if(arr[i]=='E' || arr[i]=='e'){
             num = 14;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else if(arr[i]=='F' || arr[i]=='f'){
             num = 15;
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }else{
             num = (int)arr[i]-(int)'0';
-            sprintf(str,"%04d",DecimalToBinary(num));
-            strcat(decimal,str);
+            sprintf(str,"%04ld",DecimalToBinary(num));
+            strcat(binary,str);
         }
     }
-    printf("%s",decimal);
-} 
+    long number=0;
+    for(i = 0; i < strlen(binary); i++) {
+        number = number * 10 + (binary[i] - '0');
+    }
+    
+    return number;
+    
+}
+
+long HexadecimalToDecimal(char arr[]){
+    
+    long binary = HexadecimalToBinary(arr);
+    long decimal = BinaryToDecimal(binary);
+    return decimal;
+
+}
+
+void BinaryToHexadecimal(long num){
+    long size=0;
+    long n = num;
+    long data;
+    while(n>0){
+        n = n/10;
+        size++;
+    }
+    
+    long index = 1;
+    long data;
+    n = num;
+    int i;
+    int ans;
+    while(){
+        data = n/10000;
+        for(i=1;i<=4;i++){
+            if(data%10==1){
+                
+            }    
+        }
+    }
+    
+}
 
 int main() {
-    char arr[]= {"100"};
-    HexadecimalToBinary(arr);
+    BinaryToHexadecimal(10101010);
     return 0;
 }
