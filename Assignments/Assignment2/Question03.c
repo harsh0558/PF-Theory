8b114660888c9f15d051312a8bfceffb85c64912
Harsh <hmaghnani6@gmail.com>
2024-10-26 17:16:40 +0500

    Update Question03.c

--- Assignments/Assignment2/Question03.c
+++ Assignments/Assignment2/Question03.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char arr[100];
-    char newarr[100];
-    int i=0,count=0;
-    
-    printf("enter a word: ");
-    fgets(arr,100,stdin);
-    
-    while(arr[i]!='\0'){
-        if(arr[i]!=arr[i+1]){
-            printf("%c",arr[i]);
-        }else{
-            count++;
+void compressWord(char arr[], char newarr[], int *count) {
+    int i = 0, j = 0;
+    *count = 0;
+
+    while (arr[i] != '\0' && arr[i] != '\n') { 
+        if (arr[i] != arr[i + 1]) {
+            newarr[j++] = arr[i];
+        } else {
+            (*count)++;
         }
         i++;
     }
-    printf("\n");
-    printf("%d",count);
+    newarr[j] = '\0';
+}
+
+int main() {
+    char arr[100];
+    char newarr[100];
+    int count = 0;
+
+    printf("Enter a word: ");
+    fgets(arr, 100, stdin);
+
+    compressWord(arr, newarr, &count);
     
+    printf("Compressed word: %s\n", newarr);
+    printf("Characters removed: %d\n", count);
+
     return 0;
 }
